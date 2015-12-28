828ff3cf5291775791618959bad4ce33c1e6ce09
chenluzhou <chenluzhou@users.noreply.github.com>
2015-12-28 09:34:03 +0800

    增加Hello Word程序用以验证github.com与Intellij的连接是否已经可用。 add Hello Word program to test connection with github.com and intellij is already useable.

---
+++ HelloWord/src/HelloWord.java
@@ -0,0 +1,11 @@
+/**
+ * 这个类是用来验证与github的连接是否成功。
+ * This Class is used to test the connection with github.com is already usable.
+ *
+ * Created by chenluzhou on 2015/12/28.
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PracticeToolPackage
-这是存放chenluzhou在练习时书写的代码的包。This package is used to store the code which chenluzhou done while he practice .
+这是存放chenluzhou在练习时书写的代码的包。
+This package is used to store the code which chenluzhou done while he practice .
